7ea1e240b23971fc01ad26df634bc30ed7dbe162
vitaliinnest <vitaliinnest@gmail.com>
2023-11-26 12:56:32 +0200

    fix try

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/storage-smart-contract-app/'
-})
+  base: "/storage-smart-contract-app/",
+  alias: {
+    events: "rollup-plugin-node-polyfills/polyfills/events",
+    path: "rollup-plugin-node-polyfills/polyfills/path",
+  },
+});
